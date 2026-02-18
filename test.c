e73ba1436762db1ca54558c5fbbba31d24c92987
donno2048 <just4now666666@gmail.com>
2026-02-18 21:08:18 +0000

    no need to use fcntl

--- test.c
+++ test.c
@@ -1,15 +1,14 @@
 #include <unistd.h>
 #include <pty.h>
 #include <stdlib.h>
-#include <fcntl.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <stdio.h>
 
 int fd;
 
-char buffer[7000], last_screen[6000];
-size_t screen_size, buffer_size = 0;
+char last_screen[6000];
 
 double now() {
     struct timespec ts;
@@ -18,20 +17,24 @@ double now() {
 }
 
 void write_halt(const char *w, double seconds) {
+    static char buffer[7000];
+    static size_t buffer_size = 0;
+
     write(fd, w, strlen(w));
 
     double end = now() + seconds;
     while (now() < end) {
-        size_t r = read(fd, buffer + buffer_size, 1024);
-        if (r > 0)
-            buffer_size += r;
+        size_t r_count = read(fd, buffer + buffer_size, 1024);
+        if (r_count <= 0) continue;
+        buffer_size += r_count;
         buffer[buffer_size] = '\0';
 
         char *eof = strstr(buffer + 1, "\x1b[H");
         if (eof) {
-            screen_size = eof - buffer;
+            size_t screen_size = eof - buffer;
             memcpy(last_screen, buffer, screen_size);
-            write(STDOUT_FILENO, last_screen, screen_size);
+            last_screen[screen_size] = '\0';
+            puts(last_screen);
             buffer_size -= screen_size;
             memmove(buffer, eof, buffer_size);
         }
@@ -58,9 +61,9 @@ int main() {
             write_halt("\x1b[C", 1.3);
             write_halt("\x1b[A", .7);
             write(fd, "q", 1);
-            int out = open("_test.c", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            write(out, last_screen + 11, screen_size - 11);
-            close(out);
+            FILE *out = fopen("_test.c", "w");
+            fputs(last_screen + 11, out);
+            fclose(out);
             waitpid(pid, NULL, 0);
             int status = system("gcc _test.c");
             if (status == -1 || !WIFEXITED(status) || WEXITSTATUS(status))

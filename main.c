f1f39aedd41dac881087c8e75a76a9c60d2c9d17
donno2048 <just4now666666@gmail.com>
2026-02-24 12:51:29 +0000

    use only macros

--- main.c
+++ main.c
@@ -60,9 +60,9 @@ char lines[HEIGHT][WIDTH + 1];
 char locations[MAX_LOCATIONS] = {0};
 #define NEXT_LOCATION(x) locations[x = (x + 1) % MAX_LOCATIONS]
 
-#define MAIN(x1,x2,x3,x4,x5,w,h,n,payload) \
+#define MAIN(x1,x2,x3,x4,x5,w,h,n,payloadS,playloadE) \
 int main(){\
-    payload;SETUP_SCREEN();int pX=x1,pY=x2,head=x3,tail=x4,dirX=0,dirY=0;char key=0;\
+    payloadS;SETUP_SCREEN();int pX=x1,pY=x2,head=x3,tail=x4,dirX=0,dirY=0;char key=0;\
     while(1){\
         SLEEP();GET_KEY(key);\
         switch(key){\
@@ -79,10 +79,41 @@ int main(){\
             int rX,rY;do{rX=(rand()x5(w-2))+1;rY=(rand()x5(h-2))+1;}while(lines[rY][rX]!=EMPTY);\
             lines[rY][rX]=FOOD;break;}\
         case EMPTY:{char*line=lines[n(tail)];line[n(tail)]=EMPTY;}\
-     }lines[pY][pX]=PLAYER
+     }lines[pY][pX]=PLAYER;playloadE
+#define INIT \
+    memset(lines[0], WALL, WIDTH);\
+    lines[0][WIDTH] = 0;\
+    memset(lines[HEIGHT - 1], WALL, WIDTH);\
+    lines[HEIGHT - 1][WIDTH] = 0;\
+    for (int line = 1; line < HEIGHT - 1; line++) {\
+        lines[line][0] = WALL;\
+        memset(lines[line] + 1, EMPTY, WIDTH - 2);\
+        lines[line][WIDTH - 1] = WALL;\
+        lines[line][WIDTH] = 0;\
+    }\
+    lines[HEIGHT / 4][WIDTH / 4] = FOOD
+#define END \
+        lines[pY][pX] = PLAYER;\
+        printf("%c[H%c[2J%c[3Jchar lines[%d][%d] = {", 27, 27, 27, HEIGHT, WIDTH + 1);\
+        for (int line = 0; line < HEIGHT; line++)\
+            printf("%c%c%s%c,", 10, 34, lines[line], 34);\
+        int end = MAX_LOCATIONS;\
+        while(!locations[--end]);\
+        printf("};char locations[%d]={", MAX_LOCATIONS);\
+        for (int loc = 0; loc <= end; loc++)\
+            printf("%d,", locations[loc]);\
+        char *pr = strstr(f, ";printf");\
+        printf(f, MAX_LOCATIONS, 10, 10, 10, INCLUDES_FORMAT, 10, 10, 10, 10, 34, strstr(f,\
+              "%c#define MAIN") - f, f, 34, 34, strstr(pr, "END") - pr, pr, 34, 34, 34, 10, 10, pX,\
+              pY, head, tail, 34, HEIGHT, WIDTH + 1, 34, 34, 34, 34, MAX_LOCATIONS, 34, 34, 34, 10);\
+    }\
+_exit:\
+    RESET_SCREEN();\
+    return 0;\
+}
 
 char *f = "};int m=%d,h="S(HEIGHT)",w="S(WIDTH)";%c#include<stdio.h>%c#include<stdlib.h>%c" INCLUDES
-"#define MAIN(x1,x2,x3,x4,x5) "S(MAIN(x1,x2,x3,x4,x5,w,h,NEXT,))"%c#define X(...) #__VA_ARGS__%c#de"
+"#define MAIN(x1,x2,x3,x4,x5) "S(MAIN(x1,x2,x3,x4,x5,w,h,NEXT,,))"%c#define X(...) #__VA_ARGS__%c#de"
 "fine S(x) X(x)%c#define END printf(f,m,10,10,10,"S(INCLUDES_FORMAT)",10,34,f,34,pX,pY,head,tail,34"
 ",h,w+1,34,34,34,34,m,34,34,34,10);}_exit:"S(RESET_SCREEN())";return 0;}%cchar*f=%c%.*s%%c#define N"
 "EXT(x) locations[x=(x+1)%%%%m]%%cchar*f=%%c%%s%%c;%cS(MAIN(%%d,%%d,%%d,%%d,%%%%))%c%.*s%cS(END)%c%"
@@ -91,36 +122,5 @@ char *f = "};int m=%d,h="S(HEIGHT)",w="S(WIDTH)";%c#include<stdio.h>%c#include<s
 "ne],34);printf(%c};char locations[%d]={%c);for(int loc=0;loc<m;loc++)printf(%c%%d,%c,locations[loc"
 "]);END%c";
 
-void setup() {
-     memset(lines[0], WALL, WIDTH);
-     lines[0][WIDTH] = 0;
-     memset(lines[HEIGHT - 1], WALL, WIDTH);
-     lines[HEIGHT - 1][WIDTH] = 0;
-     for (int line = 1; line < HEIGHT - 1; line++) {
-         lines[line][0] = WALL;
-         memset(lines[line] + 1, EMPTY, WIDTH - 2);
-         lines[line][WIDTH - 1] = WALL;
-         lines[line][WIDTH] = 0;
-     }
-     lines[HEIGHT / 4][WIDTH / 4] = FOOD;
-}
+MAIN(WIDTH / 2, HEIGHT / 2, 0, 0, %, WIDTH, HEIGHT, NEXT_LOCATION, INIT, END)
 
-MAIN(WIDTH / 2, HEIGHT / 2, 0, 0, %, WIDTH, HEIGHT, NEXT_LOCATION, setup());
-        lines[pY][pX] = PLAYER;
-        printf("%c[H%c[2J%c[3Jchar lines[%d][%d] = {", 27, 27, 27, HEIGHT, WIDTH + 1);
-        for (int line = 0; line < HEIGHT; line++)
-            printf("%c%c%s%c,", 10, 34, lines[line], 34);
-        int end = MAX_LOCATIONS;
-        while(!locations[--end]);
-        printf("};char locations[%d]={", MAX_LOCATIONS);
-        for (int loc = 0; loc <= end; loc++)
-            printf("%d,", locations[loc]);
-        char *pr = strstr(f, ";printf");
-        printf(f, MAX_LOCATIONS, 10, 10, 10, INCLUDES_FORMAT, 10, 10, 10, 10, 34, strstr(f,
-              "%c#define MAIN") - f, f, 34, 34, strstr(pr, "END") - pr, pr, 34, 34, 34, 10, 10, pX,
-              pY, head, tail, 34, HEIGHT, WIDTH + 1, 34, 34, 34, 34, MAX_LOCATIONS, 34, 34, 34, 10);
-    }
-_exit:
-    RESET_SCREEN();
-    return 0;
-}
